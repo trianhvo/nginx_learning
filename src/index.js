c7c5595047ebfc385c949fa7d89ae876a6114607
trianhvo <trianhvo99@gmail.com>
2024-09-21 20:51:05 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(cors());
 
+// Add this middleware
+app.use((req, res, next) => {
+  res.header('X-User-Language', req.get('X-User-Language'));
+  res.header('X-Is-Secure', req.get('X-Is-Secure'));
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
