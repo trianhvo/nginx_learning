5908e677ef99a2a90901dc3c737de3823b105774
trianhvo <trianhvo99@gmail.com>
2024-09-22 01:58:23 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () =>
-  console.log(`Example app listening on port 3000!`),
+app.listen(80, '0.0.0.0', () =>
+  console.log(`Example app listening on port 80!`)
 );
