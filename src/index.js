3c7b277dd65ba41acd35edcaa39fabc9b552cb82
trianhvo <trianhvo99@gmail.com>
2024-09-22 22:41:06 +0700

    static html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 app.use(cors());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.header('X-User-Language', req.get('X-User-Language'));
@@ -11,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(80, '0.0.0.0', () =>
